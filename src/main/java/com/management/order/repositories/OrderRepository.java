2243bac683a10219879d2846e045603b91c2dc28
t a l e s <tales.mousinho@gmail.com>
2023-12-10 21:36:08 +0000

    #5 attribute stock movements to pending orders

--- src/main/java/com/management/order/repositories/OrderRepository.java
+++ src/main/java/com/management/order/repositories/OrderRepository.java
@@ -1,11 +1,18 @@
 package com.management.order.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.management.order.entities.Item;
 import com.management.order.entities.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+  @Query("SELECT o FROM Order o WHERE o.item = :item AND o.isCompleted = false")
+  List<Order> findIncompleteOrdersForItem(Item item);
 
 }
--- src/main/java/com/management/order/services/StockMovementService.java
+++ src/main/java/com/management/order/services/StockMovementService.java
@@ -1,15 +1,75 @@
 package com.management.order.services;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.management.order.entities.Item;
+import com.management.order.entities.Order;
 import com.management.order.entities.StockMovement;
+import com.management.order.repositories.OrderRepository;
 import com.management.order.repositories.StockMovementRepository;
 
 @Service
 public class StockMovementService extends GenericCRUDService<StockMovement, Long> {
 
-  public StockMovementService(StockMovementRepository repository) {
-    super(repository);
+  @Autowired
+  private OrderRepository orderRepository;
+
+  public StockMovementService(StockMovementRepository stockMovementRepository) {
+    super(stockMovementRepository);
+  }
+
+  @Override
+  @Transactional
+  public StockMovement save(StockMovement stockMovement) {
+    Item requiredItem = stockMovement.getItem();
+    int addedQuantity = stockMovement.getQuantity();
+
+    // find all incomplete orders for the item
+    List<Order> incompleteOrders = orderRepository.findIncompleteOrdersForItem(requiredItem);
+   
+    for (Order order : incompleteOrders) {
+      // when stock is allocated, break the loop
+      if (addedQuantity <= 0) {
+        break;
+      }
+
+      int requiredQuantity = order.getQuantity() - order.getStockMovements().stream().mapToInt(StockMovement::getQuantity).sum();
+      if (requiredQuantity <= addedQuantity) {
+        // fulfill the order
+        StockMovement newStockMovement = new StockMovement();
+        newStockMovement.setItem(stockMovement.getItem());
+        newStockMovement.setQuantity(requiredQuantity);
+        newStockMovement.setOrder(order);
+        super.save(newStockMovement);
+
+        // update order status to completed and send email
+        order.setCompleted(true);
+        orderRepository.save(order);
+
+        addedQuantity -= requiredQuantity;
+      } else {
+        // partially fulfill the order
+        StockMovement newStockMovement = new StockMovement();
+        newStockMovement.setItem(stockMovement.getItem());
+        newStockMovement.setQuantity(addedQuantity);
+        newStockMovement.setOrder(order);
+        super.save(newStockMovement);
+
+        addedQuantity = 0;
+      }
+    }
+
+    if (addedQuantity > 0) {
+      // If there's still stock left, save it as a standalone stock movement
+      stockMovement.setQuantity(addedQuantity);
+      super.save(stockMovement);
+    }
+
+    return stockMovement;
   }
 
 }
