9cc0cf3c2c304a1dc056d001ad5b7afa31e219b9
t a l e s <tales.mousinho@gmail.com>
2023-12-10 21:34:31 +0000

    #4 implement Order fulfillment logic

--- src/main/java/com/management/order/repositories/StockMovementRepository.java
+++ src/main/java/com/management/order/repositories/StockMovementRepository.java
@@ -1,11 +1,18 @@
 package com.management.order.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.management.order.entities.Item;
 import com.management.order.entities.StockMovement;
 
 @Repository
 public interface StockMovementRepository extends JpaRepository<StockMovement, Long> {
 
+  @Query("SELECT s FROM StockMovement s WHERE s.item = :item AND s.order IS NULL")
+  List<StockMovement> findAvailableStockForItem(Item item);
+  
 }
--- src/main/java/com/management/order/services/OrderService.java
+++ src/main/java/com/management/order/services/OrderService.java
@@ -1,15 +1,70 @@
 package com.management.order.services;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.management.order.entities.Item;
 import com.management.order.entities.Order;
+import com.management.order.entities.StockMovement;
 import com.management.order.repositories.OrderRepository;
+import com.management.order.repositories.StockMovementRepository;
 
 @Service
 public class OrderService extends GenericCRUDService<Order, Long> {
 
-  public OrderService(OrderRepository repository) {
-    super(repository);
+  @Autowired
+  private StockMovementRepository stockMovementRepository;
+
+  public OrderService(OrderRepository orderRepository) {
+    super(orderRepository);
+  }
+
+  @Override
+  @Transactional
+  public Order save(Order order) {
+    Item requiredItem = order.getItem();
+    int requiredQuantity = order.getQuantity();
+
+    // find all available stock for the item
+    List<StockMovement> stockMovements = stockMovementRepository.findAvailableStockForItem(requiredItem);
+
+    for (StockMovement stockMovement : stockMovements) {
+      // if the order is fulfilled, break the loop
+      if (requiredQuantity <= 0) {
+        break;
+      }
+
+      int availableQuantity = stockMovement.getQuantity();
+      int remainingQuantity = 0;
+      // if there's less or equal available quantity
+      if (availableQuantity <= requiredQuantity) {
+        requiredQuantity -= availableQuantity;
+      } else {
+        // if there's more available quantity, duplicates the stock movement with the remaining quantity
+        remainingQuantity = availableQuantity - requiredQuantity;
+        StockMovement newStockMovement = new StockMovement();
+        newStockMovement.setItem(stockMovement.getItem());
+        newStockMovement.setQuantity(remainingQuantity);
+        newStockMovement.setCreationDate(stockMovement.getCreationDate());
+        stockMovementRepository.save(newStockMovement);
+        
+        requiredQuantity = 0;
+      }
+      stockMovement.setQuantity(stockMovement.getQuantity() - remainingQuantity);
+      stockMovement.setOrder(order);
+      stockMovementRepository.save(stockMovement);
+    }
+
+    // if order is fulfilled, set isCompleted to true
+    if (requiredQuantity == 0) {
+      order.setCompleted(true);
+    }
+
+    Order savedOrder = super.save(order);
+    return savedOrder;
   }
 
 }
